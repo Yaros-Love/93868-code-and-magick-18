9ad8e636ae379d1336f306c7f09e21868fb94478
Nastya <zelenenkaya@gmail.com>
2019-09-26 18:46:24 +0300

    Исправлены недочеты, массив записан в виде функции

--- js/setup.js
+++ js/setup.js
@@ -2,45 +2,37 @@
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
-
-var similarListElement = userDialog.querySelector('.setup-similar-list');
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
+var WIZARD_AMOUNT = 4;
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var getRandomIndex = function (arr) {
+var getRandomItem = function (arr) {
   var random = Math.floor(Math.random() * arr.length);
   return arr[random];
 };
 
-var wizards = [
-  {
-    name: getRandomIndex(WIZARD_NAMES) + ' ' + getRandomIndex(WIZARD_SURNAMES),
-    coatColor: getRandomIndex(COAT_COLORS),
-    eyesColor: getRandomIndex(EYES_COLORS)
-  },
-  {
-    name: getRandomIndex(WIZARD_NAMES) + ' ' + getRandomIndex(WIZARD_SURNAMES),
-    coatColor: getRandomIndex(COAT_COLORS),
-    eyesColor: getRandomIndex(EYES_COLORS)
-  },
-  {
-    name: getRandomIndex(WIZARD_NAMES) + ' ' + getRandomIndex(WIZARD_SURNAMES),
-    coatColor: getRandomIndex(COAT_COLORS),
-    eyesColor: getRandomIndex(EYES_COLORS)
-  },
-  {
-    name: getRandomIndex(WIZARD_NAMES) + ' ' + getRandomIndex(WIZARD_SURNAMES),
-    coatColor: getRandomIndex(COAT_COLORS),
-    eyesColor: getRandomIndex(EYES_COLORS)
+var createWizardsArray = function () {
+  var wizardsArray = [];
+  for (var i = 0; i < WIZARD_AMOUNT; i++) {
+    wizardsArray.push(
+      {
+        name: getRandomItem(WIZARD_NAMES) + ' ' + getRandomItem(WIZARD_SURNAMES),
+        coatColor: getRandomItem(COAT_COLORS),
+        eyesColor: getRandomItem(EYES_COLORS)
+      }
+    );
   }
-];
+  return wizardsArray;
+};
+var wizards = createWizardsArray();
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
@@ -57,6 +49,5 @@ for (var i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 
+var similarListElement = userDialog.querySelector('.setup-similar-list');
 similarListElement.appendChild(fragment);
-
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
