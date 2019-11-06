ea10db0a64cde4eff4d68994b6556c4d8a174d77
Nastya <zelenenkaya@gmail.com>
2019-11-06 13:15:25 +0300

    Загрузка аватара в диалоговое окно и иконку пользователя

---
+++ js/avatar.js
@@ -0,0 +1,31 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var fileChooser = document.querySelector('.upload input[type=file]');
+  var preview = document.querySelector('.setup-user-pic');
+  var icon = document.querySelector('.setup-open-icon');
+
+
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function(item) {
+      return fileName.endsWith(item);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+        icon.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+
+  });
+})();
