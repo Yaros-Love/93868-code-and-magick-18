cc3f2a0fae035aef5f0e821fb945653fe54fc450
Yar <grigorev.yar13@gmail.com>
2020-05-09 00:49:23 +0300

    функции создания персов и добавления их в разметку, функции универсальны

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,14 @@
 var setupModule = document.querySelector('.setup');
-setupModule.classList.remove('hidden')
+setupModule.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+//находим шаблон
+var templateWizard = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+console.log(templateWizard);
+
+//находим элемент-список персов
+var wizardList = document.querySelector('.setup-similar-list');
 
 //массив имен
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
@@ -8,36 +17,36 @@ var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Валь
 //массив цветов мантии
 var colorRoba = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 //массив цветов глаз
-var colorEyes = ['black', 'red', 'blue', 'yellow', 'green'];
+var colorEyes = ['black', 'red', 'blue', 'darkmagenta', 'crimson'];
 
 //ф-я выдачи рандомного значения из массива хар-к перса
 var randomItem = function (arrayItems) {
   return arrayItems[Math.round(Math.random() * (arrayItems.length - 1 - 0) + 0)];
 }
-//test
-console.log(randomItem(names));
-console.log(randomItem(surnames));
-console.log(randomItem(colorRoba));
-console.log(randomItem(colorEyes));
 
 //ф-я создания массива с 4мя рандомными персами
-var creationPlayers = function() {
-var playersCollection = [];
-for (i=0; i < 4; i++) {
-var player = {};
-player.name = randomItem(names);
-player.surname = randomItem(surnames);
-player.coatColor = randomItem(colorRoba);
-player.eyesColor = randomItem(colorEyes);
-playersCollection.push(player);
+var creationPlayers = function (nameArray, surnameArray,robaArray,eyesArray) {
+  var playersCollection = [];
+  for (i = 0; i < 4; i++) {
+    var player = {};
+    player.name = randomItem(nameArray);
+    player.surname = randomItem(surnameArray);
+    player.coatColor = randomItem(robaArray);
+    player.eyesColor = randomItem(eyesArray);
+    playersCollection.push(player);
+  }
+  console.log(playersCollection)
+  return playersCollection
 }
-console.log(playersCollection)
-return playersCollection
+
+//функция добавления шаблона с персами в разметку
+var creatWizards = function (playersArray) {
+  for (var i = 0; i < playersArray.length; i++) {
+    var wizardItem = templateWizard.cloneNode(true);
+    wizardItem.querySelector('.setup-similar-label').textContent = playersArray[i].name;
+    wizardItem.querySelector('.wizard-coat').style.fill = playersArray[i].coatColor;
+    wizardItem.querySelector('.wizard-eyes').style.fill = playersArray[i].eyesColor;
+    wizardList.appendChild(wizardItem)
+  }
 }
-
-
-//находим шаблон
-var templateWizard = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-console.log(templateWizard);
-
-//находим 
+creatWizards(creationPlayers(names,surnames,colorRoba,colorEyes))
